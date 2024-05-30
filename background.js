2b793466580dd209b16616e22f38daaa48394f87
Gitudon <oniku.udon.029@gmail.com>
2024-05-31 07:18:21 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,26 @@
+
+//Edge の名前空間(browser) がなけれぱ chrome の名前空間を使用する
+var browser = browser||chrome;
+
+//コンテキストメニューに拡張のメニューを追加
+browser.contextMenus.create({
+    id: 'MarkDownLink',
+    title: 'MarkDownのリンク形式で取得',
+        onclick: (info, tab)=>{ //クリックされた際のアクション
+            var url = tab.url;
+            var title = tab.title;
+            var markdown = '[' + title + '](' + url + ')';
+            toClipBoard(markdown);
+            function toClipBoard(text){
+                var ta = document.createElement("textarea");
+                ta.value = text;
+                ta.style.position = "fixed";
+                ta.style.left = "0";
+                ta.style.top = "0";
+                document.body.appendChild(ta);
+                ta.select();
+                document.execCommand("copy");
+                ta.parentElement.removeChild(ta);  
+            }
+    }
+});
